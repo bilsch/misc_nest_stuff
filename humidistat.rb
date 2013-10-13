28bc137619d5186588cf9a73916ee1a23c75c9bf
Bill Schwanitz <bilsch@bilsch.org>
2013-10-13 18:51:16 -0400

    Updated humidistat.rb
    Some time ago I changed part of the logic - never pushed it up

--- humidistat.rb
+++ humidistat.rb
@@ -16,9 +16,9 @@
 # shipping method possible. I think they are hand walking it from
 # China!
 #
-# Current relay: http://www.evilmadscience.com/productsmenu/tinykitlist/544
-# Eventual relay: http://www.amazon.com/gp/product/B009AQNBJW/ref=oh_details_o00_s00_i00
-#  - pretty sure this will work, says it'll trigger on 3v
+# Current relay: http://www.amazon.com/gp/product/B009AQNBJW/ref=oh_details_o00_s00_i00
+# Old relay: http://www.evilmadscience.com/productsmenu/tinykitlist/544
+#  - replaced the old one as it was very hacky, since I'm not using an arduino on this
 #
 
 require 'httpclient'
@@ -28,11 +28,9 @@ require 'wiringpi'
 require 'syslog'
 
 # main control variables
-debug = false
-# Note, format=json is required here
-stats_url='http://some.host.somewhere/render?from=-5minutes&until=-&target=servers.raspberrypi.sensor-RED.gauge-humidity.value%2C%22RED%22%2C%22red%22&target=servers.raspberrypi.sensor-GREEN.gauge-humidity.value%2C%22GREEN%22%2C%22green%22&target=servers.raspberrypi.sensor-BASEMENT.gauge-humidity.value%2C%22BASEMENT%22%2C%22blue%22&target=servers.home.nest-some_serial_number.gauge-humidity.value%2C%22Nest%22%2C%22brown%22&format=json'
+debug = true
 humidity_ar = []
-humidity_target = 32.0
+humidity_target = 37.0
 relay = 4 # wiringpi gpio pin 4 == gpio 23
 
 # class override found here: http://stackoverflow.com/questions/1341271/average-from-a-ruby-array
@@ -51,50 +49,59 @@ Syslog.open( 'humidistat', Syslog::LOG_PID, Syslog::LOG_DAEMON | Syslog::LOG_DAE
 Syslog.log(Syslog::LOG_INFO, "Initializing")
 
 io = WiringPi::GPIO.new
+io.mode(relay, OUTPUT)
 
-while (1)
-  # Gather nest state
-  nest = NestThermostat::Nest.new({email: "__enter_your_userid__", password: "__your_password__"})
-  device_id = nest.device_id
-  heater_state = nest.status["shared"]["#{device_id}"]["hvac_heater_state"]
-  
-  # Pull stats from graphite
-  client = HTTPClient.new()
-  stats_txt = client.get_content(stats_url)
-  stats_json = JSON.parse(stats_txt)
-  
-  # Now we need to traverse the json response and kick all of the datapoint values in to a single array
-  stats_json.each do |sensor|
-    sensor["datapoints"].each do |datapoint|
-      if datapoint[0].kind_of?(Float) || datapoint[0].kind_of?(Integer)
-        humidity_ar.push(datapoint[0])
+begin
+  while (1)
+    # Gather nest state
+    nest = NestThermostat::Nest.new({email: "__enter_your_userid__", password: "__your_password__"})
+    device_id = nest.device_id
+    heater_state = nest.status["shared"]["#{device_id}"]["hvac_heater_state"]
+    # Note, format=json is required here
+    stats_url="http://some.host.somewhere/render?from=-5minutes&until=-&target=servers.raspberrypi.sensor-RED.gauge-humidity.value%2C%22RED%22%2C%22red%22&target=servers.raspberrypi.sensor-GREEN.gauge-humidity.value%2C%22GREEN%22%2C%22green%22&target=servers.raspberrypi.sensor-BASEMENT.gauge-humidity.value%2C%22BASEMENT%22%2C%22blue%22&target=servers.home.nest-some_serial_number.gauge-humidity.value%2C%22Nest%22%2C%22brown%22&format=json"
+    
+    # Pull stats from graphite
+    client = HTTPClient.new()
+    stats_txt = client.get_content(stats_url)
+    stats_json = JSON.parse(stats_txt)
+    
+    # Now we need to traverse the json response and kick all of the datapoint values in to a single array
+    stats_json.each do |sensor|
+      sensor["datapoints"].each do |datapoint|
+        if datapoint[0].kind_of?(Float) || datapoint[0].kind_of?(Integer)
+          humidity_ar.push(datapoint[0])
+        end
       end
     end
-  end
-  
-  # now we make some logic decisions
-  avg_humidity = humidity_ar.mean
-  if avg_humidity <= humidity_target
-    if heater_state
-      Syslog.log(Syslog::LOG_INFO, "Turn on the humidifier heater_state: #{heater_state} - #{avg_humidity} below target of #{humidity_target}")
-      io.write(relay, HIGH)
-      sleep(30)
-      io.write(relay, LOW)
+    
+    # now we make some logic decisions
+    avg_humidity = humidity_ar.mean
+    if avg_humidity <= humidity_target
+      if heater_state
+        Syslog.log(Syslog::LOG_INFO, "Turn on the humidifier heater_state: #{heater_state} - #{avg_humidity} below target of #{humidity_target}")
+        io.write(relay, HIGH)
+        #sleep(30)
+        #io.write(relay, LOW)
+      else
+        if debug 
+          Syslog.log(Syslog::LOG_DEBUG, "Would turn on but Heater not running heater_state: #{heater_state} - #{avg_humidity} below target of #{humidity_target}")
+        end
+      end
     else
-      if debug 
-        Syslog.log(Syslog::LOG_DEBUG, "Would turn on but Heater not running heater_state: #{heater_state} - #{avg_humidity} below target of #{humidity_target}")
-      end
-    end
-  else
-    if debug 
       Syslog.log(Syslog::LOG_DEBUG, "Leave it off - #{avg_humidity} >= target of #{humidity_target}")
+      io.write(relay, LOW)
     end
-    io.write(relay, LOW)
+  
+    # Pulse the humidifier on/off in the loop so we don't waste too much water
+    if debug 
+      Syslog.log(Syslog::LOG_DEBUG, "loop")
+    end
+    if ! heater_state
+      io.write(relay, LOW)
+    end
+    sleep(30)
   end
-
-  # Pulse the humidifier on/off in the loop so we don't waste too much water
-  if debug 
-    Syslog.log(Syslog::LOG_DEBUG, "loop")
-  end
-  sleep(30)
+rescue => e
+  Syslog.log(Syslog::LOG_ERR, "Caught exception #{e.message}")
+  Syslog.log(Syslog::LOG_ERR, "Caught exception #{e.backtrace}")
 end
